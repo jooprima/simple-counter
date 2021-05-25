41f559000b77ae1454c0c841c48bbed9b733a10a
jooprima <jooprima@gmail.com>
2021-05-25 22:10:12 +0700

    craete navigations element

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ function App() {
         <img className="nav-icon" src={shoppingIcon} alt="shopping icon" />
         <h1 className="nav-title">Shopping List</h1>
       </nav>
+
+      <section className="container">
+        
+      </section>
     </>
   );
 }
