e16f866626344bc8b1192be732edec28c99ee9f2
jooprima <jooprima@gmail.com>
2021-05-25 15:11:33 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+// import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Plus
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Minus
+      </button>
+    </div>
+  );
+}
+
+export default App;
