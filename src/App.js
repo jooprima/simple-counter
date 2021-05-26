1b4682e555390f64cf4b72e0bb4e6d37d4f538ff
jooprima <jooprima@gmail.com>
2021-05-26 21:10:13 +0700

    create todo elemetns

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import classnames from "classnames";
 
 import shoppingIcon from "./assets/shopping-icon.svg";
+import plusIcon from "./assets/plus-icon.svg";
+import minusIcon from "./assets/minus-icon.svg";
 
 import "./App.css";
 
@@ -37,11 +40,24 @@ function App() {
 
         {todos.length > 0 ? (
           <div className="todos">
-            {todos.map((todo) => {
+            {todos.map((todo, index, arr) => {
               return (
-                <div>
+                <div
+                  key={index}
+                  className={`todo ${
+                    !(arr.length === index + 1) && "todo-divider"
+                  }`}
+                >
                   {todo.title}
-                  {todo.count}
+                  <div className="todo-icon-wrapper">
+                    <div className="todo-count">{todo.count}</div>
+                    <button className="todo-action-button">
+                      <img src={minusIcon} alt="minus icon" />
+                    </button>
+                    <button className="todo-action-button">
+                      <img src={plusIcon} alt="plus icon" />
+                    </button>
+                  </div>
                 </div>
               );
             })}
